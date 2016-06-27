7cfffef258cb4fd69800ae78cd5f0e6c5498bfe3
Jeremy Moore <damoores@gmail.com>
2016-06-27 14:11:06 -0700

    initial project setup

---
+++ GoGoGadgetGithub/GithubOAuth.swift
@@ -0,0 +1,10 @@
+//
+//  GithubOAuth.swift
+//  GoGoGithub
+//
+//  Created by Jeremy Moore on 6/27/16.
+//  Copyright © 2016 Jeremy Moore. All rights reserved.
+//
+
+import UIKit
+
---
+++ GoGoGadgetGithub/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GoGoGadgetGithub
+//
+//  Created by Jeremy Moore on 6/27/16.
+//  Copyright © 2016 Jeremy Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
